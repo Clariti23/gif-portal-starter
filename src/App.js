577a161a1655c456737792059e162051b65342c1
Grant <g.ferowich@gmail.com>
2022-09-28 20:04:17 -0500

    mapped image grid with conditional rendering logic

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ import './App.css';
 // Constants
 const TWITTER_HANDLE = 'GrantFerowich';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-
+const DUMMY_GIFS = [
+  'https://media.giphy.com/media/DvbEI4LAFaZaw/giphy.gif',
+  'https://media.giphy.com/media/3oriOi8MuvRXzz3tHG/giphy.gif',
+  'https://media.giphy.com/media/UFXi3FSeGtKq4/giphy.gif',
+  'https://media.giphy.com/media/l2SpNtHdgOA9s1qNy/giphy.gif',
+  'https://media.giphy.com/media/3qj3VtNL2nhmw/giphy.gif',
+  'https://media.giphy.com/media/3o6vXOloJUr5dFQmQ0/giphy.gif',
+  'https://media.giphy.com/media/l2RnnnALPxrKmQq1q/giphy.gif',
+  'https://media.giphy.com/media/3oEhn51EPgFiSQw8GQ/giphy.gif'
+]
 const App = () => {
   // State
   const [ walletAddress, setWalletAddress ] = useState(null);
+ 
+
   
   const checkIfWalletIsConnected = async () => {
     try {
@@ -49,6 +60,18 @@ const App = () => {
     </button>
   );
 
+  const renderConnectedContainer = () => (
+    <div className='connected-container'>
+      <div className='gif-grid'>
+        {DUMMY_GIFS.map( gif =>(
+          <div className='gif-item' key={gif}>
+            <img src={gif} alt={gif}/>
+          </div>  
+        ))}
+      </div>
+    </div>
+  )
+
   useEffect(() => {
     const onLoad = async () => {
       await checkIfWalletIsConnected();
@@ -61,12 +84,14 @@ const App = () => {
     <div className="App">
       <div className={walletAddress ? 'authed-container':"container"}>
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">🖼 The Great GIF Portal</p>
           <p className="sub-text">
             by Grant J.F. Ferowich ✨
           </p>
           { !walletAddress && renderNotConnectedContainer()}
+          { walletAddress && renderConnectedContainer()}
         </div>
+
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
